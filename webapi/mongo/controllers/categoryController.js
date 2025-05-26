0b4eb728c6927f7a8285429edc6681a32ccbe78c
xhuong278 <huongphxps39408@gmail.com>
2025-05-26 20:22:46 +0700

    done cate

--- webapi/mongo/controllers/categoryController.js
+++ webapi/mongo/controllers/categoryController.js
@@ -1,7 +1,6 @@
 const categoriesModel = require('../models/categoryModel');
-module.exports = { getAllCate};
+const productsModel = require('../models/productsModel');
 
-// Lấy toàn bộ danh mục
 async function getAllCate() {
     try {
         return await categoriesModel.find();
@@ -10,3 +9,75 @@ async function getAllCate() {
         throw new Error('Lỗi lấy dữ liệu danh mục');
     }
 }
+
+async function getCateById(id) {
+    try {
+        const category = await categoriesModel.findById(id);
+        if (!category) throw new Error('Danh mục không tồn tại');
+        return category;
+    } catch (error) {
+        console.error('Lỗi lấy chi tiết danh mục:', error.message);
+        throw new Error('Lỗi lấy chi tiết danh mục');
+    }
+}
+
+async function addCate(data) {
+    try {
+        const { name, slug, children } = data;
+        if (!name) throw new Error('Tên danh mục không được để trống');
+        if (!slug) throw new Error('Slug danh mục không được để trống');
+
+        const newCate = new categoriesModel({
+            name,
+            slug,
+            children: children || []
+        });
+
+        return await newCate.save();
+    } catch (error) {
+        console.error('Lỗi thêm danh mục:', error.message);
+        throw new Error('Lỗi thêm danh mục');
+    }
+}
+
+async function updateCate(id, data) {
+    try {
+        const { name, slug, children } = data;
+        const category = await categoriesModel.findById(id);
+        if (!category) {
+            throw new Error('Danh mục không tồn tại');
+        }
+
+        category.name = name || category.name;
+        category.slug = slug || category.slug;
+        
+        // Nếu có truyền children lên thì ghi đè luôn
+        if (Array.isArray(children)) {
+            category.children = children;
+        }
+
+        return await category.save();
+    } catch (error) {
+        console.error('Lỗi cập nhật danh mục:', error.message);
+        throw new Error('Lỗi cập nhật danh mục');
+    }
+}
+
+
+async function deleteCate(id) {
+    try {
+        const cate = await categoriesModel.findById(id);
+        if (!cate) throw new Error('Không tìm thấy danh mục');
+
+        const pros = await productsModel.find({ 'cate_id.categoryId': id });
+        if (pros.length > 0) throw new Error('Danh mục có sản phẩm không thể xóa');
+
+        const result = await categoriesModel.findByIdAndDelete(id);
+        return result;
+    } catch (error) {
+        console.error('Lỗi xóa danh mục:', error.message);
+        throw new Error('Lỗi xóa danh mục');
+    }
+}
+
+module.exports = { getAllCate, getCateById, addCate, updateCate, deleteCate };
