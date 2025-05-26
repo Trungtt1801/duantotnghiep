b5da920363471077274b4cc1df8d0904938ea2aa
Trungtt1801 <truongtritrung1801@gmail.com>
2025-05-26 18:50:15 +0700

    test

--- webapi/app.js
+++ webapi/app.js
@@ -8,10 +8,8 @@ const mongoose = require('mongoose');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var productRouter = require('./routes/products');
-<<<<<<< HEAD
-=======
 var categoryRouter = require('./routes/category');
->>>>>>> Hương
+
 
 var app = express();
 
@@ -28,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter); 
 app.use('/products', productRouter); 
+app.use('/category', categoryRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
