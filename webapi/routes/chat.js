8edc88b6c1513999a1af5111bcd1a27971bdf2b4
Trungtt1801 <truongtritrung1801@gmail.com>
2025-07-01 17:22:55 +0700

    nothing

--- webapi/routes/chat.js
+++ webapi/routes/chat.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { chatWithBot } = require('../mongo/controllers/chatController');
 
+// http://localhost:3000/chat
 router.post('/', chatWithBot);
 
 module.exports = router;
