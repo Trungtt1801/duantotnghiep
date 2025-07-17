efaeebae6fb858cf841a3e268a40a620cd75f54f
Trungtt1801 <truongtritrung1801@gmail.com>
2025-07-18 00:53:08 +0700

    update app.js

--- webapi/app.js
+++ webapi/app.js
@@ -28,8 +28,9 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
 
 app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/images", express.static(path.join(__dirname, "public/images")));
