a27c24695771ef7da2c818daa92e9f53c4837386
Vinh-Dang21 <123trongvinh456@gmail.com>
2025-07-25 03:59:34 +0700

    review

--- webapi/mongo/models/reviewModel.js
+++ webapi/mongo/models/reviewModel.js
@@ -2,9 +2,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const reviewSchema = new Schema({
-  order_detail_id: { type: Schema.Types.ObjectId, ref: "orderDetail", required: true, unique: true }, // duy nhất để mỗi sản phẩm chỉ được đánh giá 1 lần/đơn
-  product_id: { type: Schema.Types.ObjectId, ref: "products", required: true },
-  user_id: { type: Schema.Types.ObjectId, ref: "users", required: true },
+user_id: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+product_id: { type: mongoose.Schema.Types.ObjectId, ref: "products", required: true },
   rating: { type: Number, required: true },
   content: { type: String },
   images: [String],
