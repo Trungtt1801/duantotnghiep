37cb4e937e3b25eb8f3e5f227e6dc00b8d737828
Vinh-Dang21 <123trongvinh456@gmail.com>
2025-07-29 16:10:23 +0700

    Update orderModel.js

--- webapi/mongo/models/orderModel.js
+++ webapi/mongo/models/orderModel.js
@@ -8,7 +8,7 @@ const OrderSchema = new Schema(
     status_order: {
       type: String,
       enum: ["pending", "preparing", "awaiting_shipment", "shipping", "delivered","failed", "cancelled", "refund"],
-      required: false,
+      required: true,
     },
     status_history: [
       {
